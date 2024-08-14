595729fc50d1e0e53186409a3c8f57a9db29dddc
Elvin German <ElvinG13@nycstudents.net>
2024-08-14 06:28:49 +0000

    added quote system

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let dark = document.querySelector("button")
+let dark = document.getElementById("DMode")
 function darkMode(){
     document.body.style.backgroundColor = `rgb(0,0,0)`
 }
 dark.addEventListener("click", darkMode)
+
+let Rain = document.getElementById("quote")
+function genRanQot(){
+    const Quotes = ["'Quote1'", "Quote2", "Quote3", "Quote4",];
+    const RandomIndex = Math.floor(Math.random()*Quotes.length);
+    document.getElementById("quote").textContent = Quotes[RandomIndex];
+}
+document.getElementById("NewButton").addEventListener("click", genRanQot)
