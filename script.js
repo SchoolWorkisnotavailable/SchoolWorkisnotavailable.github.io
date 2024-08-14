58b126d7dd6cef89dded153c621af1fc0cc79ee9
Elvin German <ElvinG13@nycstudents.net>
2024-08-14 15:38:44 +0000

    last update (for now): added refinements

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ dark.addEventListener("click", darkMode)
 
 let Rain = document.getElementById("quote")
 function genRanQot(){
-    const Quotes = ["'Quote1'", "Quote2", "Quote3", "Quote4",];
+    const Quotes = ["The secret of getting ahead is getting started", "The very things that holdyou down are going to lift you up.", "'When life give you lemons, make lemonade", "The price of greatness is responsibility",];
     const RandomIndex = Math.floor(Math.random()*Quotes.length);
     document.getElementById("quote").textContent = Quotes[RandomIndex];
 }
